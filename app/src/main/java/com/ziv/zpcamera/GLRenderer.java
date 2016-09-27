829128aa62c4b31c7ce370089afe7cea27af0e90
zhaopeng <Ziv_Android@Outlook.com>
2016-09-27 20:32:35 +0800

    测试全屏状态标题栏与状态栏的隐藏

--- app/src/main/java/com/ziv/zpcamera/GLRenderer.java
+++ app/src/main/java/com/ziv/zpcamera/GLRenderer.java
@@ -16,6 +16,7 @@ public class GLRenderer implements GLSurfaceView.Renderer {
     /**
      * Called when the surface is created or recreated.
      * 当GLSurfaceView初始创建时、设备唤醒时、重新启动该Activity时被回调
+     * 在这个方法中主要用来设置一些绘制时不常变化的参数，比如：背景色
      */
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
@@ -34,6 +35,7 @@ public class GLRenderer implements GLSurfaceView.Renderer {
     /**
      * Called when the surface changed size.
      * 当GLSurfaceView初始创建时，或者GLSurfaceView的大小发生改变时，比如设备旋转屏幕
+     * 如果设备支持屏幕横向和纵向切换，在横向<->纵向互换时，该方法可以重新设置绘制的纵横比率
      */
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -43,6 +45,7 @@ public class GLRenderer implements GLSurfaceView.Renderer {
     /**
      * Called to draw the current frame.
      * 会被GL线程循环使用，每循环一次，就会将内容绘制在屏幕上
+     * 定义实际的绘图操作
      */
     @Override
     public void onDrawFrame(GL10 gl) {
--- app/src/main/java/com/ziv/zpcamera/MainActivity.java
+++ app/src/main/java/com/ziv/zpcamera/MainActivity.java
@@ -3,9 +3,11 @@ package com.ziv.zpcamera;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-    // 窗口
+    // 声明窗口
     private GLSurfaceView glSurfaceView;
     // 判断GL是否开始运行
     private boolean hasRenderer;
@@ -13,23 +15,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 设置全屏
+        // 隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // 隐藏标题栏
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
 
         glSurfaceView = new GLSurfaceView(this);
-        // 使用ActivityManager的getDeviceConfigureationInfo().reqGLEsVersion
-        // 如果大于0x20000，则支持OpenGL ES2.0
-
+        // 使用ActivityManager的getDeviceConfigureationInfo().reqGLEsVersion获取OpenGL ES版本信息
         // 为GLSurfaceView的窗口配置OpenGL ES2.0的上下文信息
         glSurfaceView.setEGLContextClientVersion(2);
         // 为GLSurfaceView的窗口配置渲染线程
         glSurfaceView.setRenderer(new GLRenderer());
-
         // 优化选项
         // RENDERMODE_WHEN_DIRTY    只在创建和调用requestRender时才会重新绘制
         // RENDERMODE_CONTINUOUSLY  始终重绘
         glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
 
         hasRenderer = true;
-
         setContentView(glSurfaceView);
     }
 
